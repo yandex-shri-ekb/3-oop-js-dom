5a1ac679d366d53e9e7e1b51a26ec16f5e295e1b
Vladimir <metenevvv@gmail.com>
2013-10-10 00:55:27 +0600

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
 /*jslint plusplus: true, vars: true, browser: true, devel: true */
-var Genarator = function (text) {
+function Generator (text) {
     var wordSForDictionary = [],
     currentPair;
     //function rndNumber
@@ -13,23 +13,7 @@ var Genarator = function (text) {
       }
       return wordSForDictionary;
     },
-    this.fillDictionary = function(Dictionary) {
-      var lengthDictionary = wordSForDictionary.length;
-      
-      outer:
-      for(var i=0; i<lengthDictionary; i+=1) {
-        if(wordSForDictionary[i+2]==undefined){
-          Dictionary[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push('#');
-          break outer;
-        }
-        if(wordSForDictionary[i]+' '+wordSForDictionary[i+1] in Dictionary) {
-          Dictionary[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push(wordSForDictionary[i+2])
-          continue;
-        }
-        Dictionary[wordSForDictionary[i]+' '+wordSForDictionary[i+1]] =[];
-        Dictionary[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push(wordSForDictionary[i+2]);
-      }
-    },
+    
     this.createText = function(Dictionary) {
       
     },
@@ -39,69 +23,56 @@ var Genarator = function (text) {
 
 
 
-Generate.prototyep.getRndWord = function (wordsMassiv) {
-    //if(wordsMassiv.length === 0) {
-//      var word = ' .';
-
-  //    return word;
-  //  }
+Generate.prototype.getRndWord = function (wordsMassiv) {
     var rndKey = Math.floor(wordsMassiv.length * Math.random() )
-   // var word = wordsMassiv[rndKey];
-
     return rndKey;
-}
+  }
 
 
 
-function createProposal(Dictionary, a, b) {
+Generate.prototype.createProposal = function (Dictionary, a, b) {
     "use strict";
     var tempMas = [],
         proposal = [],
         words = [],
-        word,
         lengthProposal,
         findingLength,
-        rndKey,
         firstPair;
 
     for (var i=a; i<=b; i+=1) {
       tempMas.push(a);
     }
     
-    rndKey = getRndWord(tempMas);
-    findingLength = tempMas[rndKey];
-    tempMas = null;
+var iterator = new Iterator(tempMas);
+
+    findingLength = iterator.randomItem();
+    tempMas.length = 0;
   
-    for(var key in Dictionary) {
+    for(var key in Dictionary.text) {
       tempMas.push(key);
     }
-    firstPair = getRndWord(tempMas);
+
+    firstPair = iterator.randomItem();
     words = firstPair.split(' ');
     
     while( words[0].match( /[.!?#]/ ) && words[1].match( /[.!?#]/ ) ) {
-      firstPair = getRndWord(tempMas);
+      firstPair = iterator.randomItem();
       words = firstPair.split(' ');
     }
 
-    tempMas = null;
+    tempMas.length = 0;
     
     proposal.push(words[0], words[1]);
     words = null;
     findingLength = proposal.length;
     while ( findingLength !== lengthProposal) {
       word = getNextWord(proposal);
-      //checkWords(proposal);
       proposal.push(word);
     }
     return proposal;
 }
 
 function getNextWord(proposal) {
-  //  var check = checkWidthProposal(proposal);
-  //  if(check) {
-  //    
-  //    return;
-  //  }
     var lengthProposal = proposal.length,
         lastWord = proposal[lengthProposal],
         beforeLastWord =  proposal[lengthProposal - 1],
@@ -112,12 +83,11 @@ function getNextWord(proposal) {
         maxCount = 50;
         word;
 
-    masWords = Dictionary[currentPair];
-    rndKey = getRndWord(masWords);
-    word = masWords[rndKey];
+    masWords = Dictionary.text[currentPair];
+    var iterator = new Iterator(masWords);
+    word = iterator.randomItem;
     while (word.match( /[.!?#]/ ) && count < maxCount) {
-      rndKey = getRndWord(masWords);
-      word = masWords[rndKey];
+      word = iterator.randomItem;
       count++;
     }
     if (count === maxCount) {
@@ -126,6 +96,56 @@ function getNextWord(proposal) {
     return word;
 }    
 
-function Dictionary() {
-    this.getFirst
+
+var Iterator = function(items) {
+    this.index = 0;
+    this.items = items;
 }
+
+Iterator.prototype = {
+    first: function() {
+      this.reset();
+      return this.next();
+    },
+    randomItem: function() {
+      var rndKey = Math.floor(this.items.length * Math.random() )
+      return this.items[rndKey];
+    },    
+    next: function() {
+      return this.items[this.index++];
+    },
+    hasNext: function() {
+      return this.index <= this.items.length;
+    },
+    reset: function() {
+      this.index = 0;
+    },
+    each: function(callback) {
+      for(var item = this.first(); this.hasNext(); item = this.next() ){
+        callback(item);
+      }
+    }
+}
+
+function Dictionary(wordSForDictionary) {
+    this.text = {};
+    this.authorComment = [];
+}
+Dictionary.prototype.fillDictionary = function(wordSForDictionary) {
+      var lengthDictionary = wordSForDictionary.length;
+      
+      outer:
+      for(var i=0; i<lengthDictionary; i+=1) {
+        if(wordSForDictionary[i+2]==undefined){
+          this.text[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push('#');
+          break outer;
+        }
+        if(wordSForDictionary[i]+' '+wordSForDictionary[i+1] in this.text) {
+          this.text[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push(wordSForDictionary[i+2])
+          continue;
+        }
+        this.text[wordSForDictionary[i]+' '+wordSForDictionary[i+1]] =[];
+        this.text[wordSForDictionary[i]+' '+wordSForDictionary[i+1]].push(wordSForDictionary[i+2]);
+      }
+    },
+
